b411df14d3d75c3e05868d0ce77eeb9177b65ec8
anmol11909 <117017384+anmol11909@users.noreply.github.com>
2022-12-12 22:33:32 +0530

    rollar coaster elif command

--- rollar coaster.py
+++ rollar coaster.py
@@ -1,6 +1,14 @@
+#if /elif/else
 print("welcome to the rollar coaster")
 height = int(input("what is your height"))
-if height>190:
+if height>=130:
   print("Congratulations you can ride")
+  age = int(input("What is your age"))
+  if age<12:
+    print("You have to pay $5")
+  elif age <= 18:
+    print("You have to pay $10")
+  else:
+    print("You have to pay $15")
 else:
   print("sorry you are not tall enough for this ride, you can go for some other ride")
